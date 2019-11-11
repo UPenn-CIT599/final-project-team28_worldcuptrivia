ba05bb5943abf3b6b176953f5d297e26d2e732da
pierrepmourier <pmourier@seas.upenn.edu>
2019-11-11 12:29:11 -0500

    some sample methods stubs

--- src/worldcup_trivia/FactsAnalysis.java
+++ src/worldcup_trivia/FactsAnalysis.java
@@ -6,9 +6,37 @@ package worldcup_trivia;
 
 public class FactsAnalysis {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	/**
+	 * This method computes the game with the most goals
+	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @return the id of the game with the most goals
+	 */
+	public int gameWithMostGoals(WorldCup wc) {
+		}
+	
+	/**
+	 * This method computes the game with the most goals
+	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @return the name of the team with the most goals
+	 */
+	public String bestOffense(WorldCup wc) {
+		}
+	
+	/**
+	 * This method computes the game with least goals against
+	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @return the name of the team with the least goals against
+	 */
+	public String bestDefense(WorldCup wc) {
+		}
+	
+	/**
+	 * This method computes the average accuracy for a given team
+	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @param team a team name
+	 * @return the $ accuracy for given team at a given world cup
+	 */
+	public double averageAccuracy(WorldCup wc, String team) {
+		}
 
 }
