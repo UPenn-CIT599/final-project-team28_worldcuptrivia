f6a3cad9099c52074786e8968b8c3f3e73cd71fd
pierrepmourier <pmourier@seas.upenn.edu>
2019-11-11 12:52:45 -0500

    added javadocs and todo steps for each method

--- src/worldcuptrivia/SoccerAnalysis.java
+++ src/worldcuptrivia/SoccerAnalysis.java
@@ -12,6 +12,11 @@ public class SoccerAnalysis {
 	 * @return the id of the game with the most goals
 	 */
 	public int gameWithMostGoals(WorldCup wc) {
+		int id = 0;
+		// create a hashmap for games and total goals
+		// loop through all games, add-up goals for and against and add to hashmaps
+		// return the game id for the game with max goals
+		return id;
 		}
 	
 	/**
@@ -20,6 +25,11 @@ public class SoccerAnalysis {
 	 * @return the name of the team with the most goals
 	 */
 	public String bestOffense(WorldCup wc) {
+		String team = null;
+		// create a hashmap for teams and total goals
+		// loop through all games of a given team, add-up goals for add to hashmaps
+		// return the team name for the team with most goals
+		return team;
 		}
 	
 	/**
@@ -28,15 +38,25 @@ public class SoccerAnalysis {
 	 * @return the name of the team with the least goals against
 	 */
 	public String bestDefense(WorldCup wc) {
+		String team = null;
+		// create a hashmap for teams and total goals
+		// loop through all games of a given team, add-up goals against add to hashmaps
+		// return the team name for the team with least goals
+		return team;
 		}
 	
 	/**
-	 * This method computes the average accuracy for a given team
+	 * This method computes team should should see if you want to see woodwork
 	 * @param wc a WorldCup object, itself an ArrayList of Games objects
 	 * @param team a team name
-	 * @return the % accuracy for given team at a given world cup
+	 * @return the team name
 	 */
-	public double averageAccuracy(WorldCup wc, String team) {
+	public String mostWoodWork(WorldCup wc, String team) {
+		String team = null;
+		// create a hashmap for teams and total woodwork
+		// loop through all games of a given team, add-up woodwork add to hashmaps
+		// return the team name for the team with most wwodwork
+		return team;
 		}
 
 }
