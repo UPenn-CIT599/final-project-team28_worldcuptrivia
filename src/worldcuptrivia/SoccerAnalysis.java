c6adfe3b06dcef76a6af36461befcbdf995bad68
pierrepmourier <pmourier@seas.upenn.edu>
2019-11-11 13:04:39 -0500

    create stub game object and updated SoccerAnalysis accordingly

--- src/worldcuptrivia/SoccerAnalysis.java
+++ src/worldcuptrivia/SoccerAnalysis.java
@@ -4,14 +4,24 @@ This will be the main analysis class
 
 package worldcuptrivia;
 
+import java.util.ArrayList;
+
 public class SoccerAnalysis {
+	
+	// instance variables
+	ArrayList<Game> games;
+
+	// constructor
+	public SoccerAnalysis(ArrayList<Game> someGames) {
+		this.games = someGames;
+	}
 
 	/**
 	 * This method computes the game with the most goals
-	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @param someGames an ArrayList of Games objects
 	 * @return the id of the game with the most goals
 	 */
-	public int gameWithMostGoals(WorldCup wc) {
+	public int gameWithMostGoals(ArrayList<Game> someGames) {
 		int id = 0;
 		// create a hashmap for games and total goals
 		// loop through all games, add-up goals for and against and add to hashmaps
@@ -22,10 +32,10 @@ public class SoccerAnalysis {
 	
 	/**
 	 * This method computes the team with the most goals
-	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @param someGames an ArrayList of Games objects
 	 * @return the name of the team with the most goals
 	 */
-	public String bestOffense(WorldCup wc) {
+	public String bestOffense(ArrayList<Game> someGames) {
 		String team = null;
 		// create a hashmap for teams and total goals
 		// loop through all games of a given team, add-up goals for add to hashmaps
@@ -35,10 +45,10 @@ public class SoccerAnalysis {
 	
 	/**
 	 * This method computes the team with least goals against
-	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @param someGames an ArrayList of Games objects
 	 * @return the name of the team with the least goals against
 	 */
-	public String bestDefense(WorldCup wc) {
+	public String bestDefense(ArrayList<Game> someGames) {
 		String team = null;
 		// create a hashmap for teams and total goals
 		// loop through all games of a given team, add-up goals against add to hashmaps
@@ -48,10 +58,10 @@ public class SoccerAnalysis {
 	
 	/**
 	 * This method computes team should should see if you want to see woodwork
-	 * @param wc a WorldCup object, itself an ArrayList of Games objects
+	 * @param someGames an ArrayList of Games objects
 	 * @return the team name
 	 */
-	public String mostWoodWork(WorldCup wc) {
+	public String mostWoodWork(ArrayList<Game> someGames) {
 		String team = null;
 		// create a hashmap for teams and total woodwork
 		// loop through all games of a given team, add-up woodwork add to hashmaps
