258c8510781a631dbad7f1c4c538349b40734f80
pierrepmourier <pmourier@seas.upenn.edu>
2019-11-22 17:10:34 -0500

    adding our skill ID

--- src/com/amazon/ask/airplanefacts/WorldCupTriviaStreamHandler.java
+++ src/com/amazon/ask/airplanefacts/WorldCupTriviaStreamHandler.java
@@ -19,7 +19,7 @@ public class WorldCupTriviaStreamHandler extends SkillStreamHandler {
                         new AlexaHandlerEnd(),
                         new AlexaHandlerFallback())
                 // Add your skill id below and uncomment to enable skill ID verification
-                // .withSkillId("")
+                .withSkillId("amzn1.ask.skill.6f264775-f5f9-4c66-8b0f-ccd1682733f8")
                 .build();
     }
 
